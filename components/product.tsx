1493e388194a5e95ae9de189f4e195f52c917ce7
Andrey <starline@ya.ru>
2022-08-06 15:58:02 +0300

    add state in component

--- components/product.tsx
+++ components/product.tsx
@@ -2,15 +2,45 @@ import Link from "next/link";
 import React from "react";
 import { IProductProps } from "../interfaces/product";
 
-export class Product extends React.Component<IProductProps> {
+interface IState {
+    date?: Date;
+  }
+
+export class Product extends React.Component<IProductProps, IState> {
     
+    timerID: NodeJS.Timer;
+
+    constructor(props:IProductProps){
+        super(props)
+        this.state = {date: new Date()}
+    }
+    
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(), 1000
+        )
+    }
+    
+    tick(){
+        this.setState({
+            date:new Date()
+        })
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID)
+    }
+
     render() {
         return (
-            <div className='product_item'>
-                <Link href={`/products/${this.props.product.id}`}>
-                    <a>{this.props.product.id} {this.props.product.title}</a>
-                </Link>
-            </div>
+            <Link href={`/products/${this.props.product.id}`}>
+                <a>
+                    <div className='product_item'>
+                        <div className="product_item_name">{this.props.product.id} {this.props.product.title}</div>
+                        <div className="product_item_time">{this.state.date.toLocaleTimeString()}</div>
+                    </div>
+                </a>                
+            </Link>  
         )
     }
 }
